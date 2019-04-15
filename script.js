88751f36635b943327c04f9d862eac1314ef0eec
wardhai <46731990+wardhai@users.noreply.github.com>
2019-04-15 14:11:40 -0500

    updating

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
- var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 41.831613, lng: -87.627294},
+  function initMap() {
+	var tower = {lat: 41.879185, lng: -87.635772}    
+    var map = new google.maps.Map(document.getElementById('map'), {
+          center: tower,
           zoom: 8
         });
-      }
+		
+	var marker = new google.maps.Marker({
+		  position: tower,
+		  map: map,
+		  animation: google.maps.Animation.BOUNCE,
+		  icon: 'images/marker.png'
+	    });
+		
+	var infowindow = new google.maps.InfoWindow({
+		  content: 'Willis Tower: Skydeck'
+	    });
+		
 
+      
+ google.maps.event.addListener(marker, 'mouseover', function() {
+    	infowindow.open(myMap, marker);
+  	});
+}
